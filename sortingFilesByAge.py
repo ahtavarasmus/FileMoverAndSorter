090159b1118fc85e9788d047712007ba81513cef
ahtavarasmus <76125307+ahtavarasmus@users.noreply.github.com>
2021-07-10 18:17:10 +0300

    main

---
+++ sortingFilesByAge.py
@@ -0,0 +1,39 @@
+import os
+import time
+import datetime
+from pathlib import Path
+import shutil
+
+
+def movingTheFile(og_path):
+
+    # getting the last modified time as a datetime:
+    # mtime = 1585501464.0
+    mtime = os.path.getmtime(og_path)
+    date = time.ctime(mtime)  # date = Sun Mar 29 20:04:24 2020'
+
+    # getting the datetime object
+    # datetime.datetime(2020, 3, 29, 20, 4, 24)
+    datetimeObj = datetime.datetime.strptime(date, '%a %b %d %H:%M:%S %Y')
+
+    # Getting a year from datetimeObj
+    year = datetimeObj.year  # year = 2020
+    month = datetimeObj.strftime('%B')  # month = 3
+
+    filename = os.path.basename(full_path)
+    newPath = 'D:\\sortedtest1\\{}\\{}'.format(year, month)
+
+    Path(newPath).mkdir(parents=True, exist_ok=True)  # windowsPath object
+
+    newfilePath = os.path.join(newPath, filename)
+    print('Copying {} to {}'.format(full_path, newfilePath))
+    #shutil.copy(full_path, newfilePath)
+
+
+downloadThreads = []
+
+
+for foldername, subfolders, filenames in os.walk('C:\\Users\\ahtav\\Videos\\Fortnite'):
+    for filename in filenames:
+        full_path = os.path.join(foldername, filename)
+        movingTheFile(full_path)
